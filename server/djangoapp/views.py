5a1c7e4ddf865a2061c7fb8118e2301098f7e87c
Sebastian Kotulla <82386236+DerSimulant@users.noreply.github.com>
2023-07-27 18:43:29 +0200

    Update views.py
    updated dealership

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -87,11 +87,17 @@ def register(request):
         return render(request, 'djangoapp/registration.html')
 
 
+
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+        url = "https://fae2e570-c462-4ebb-adc6-2e0936c618d7-bluemix.cloudantnosqldb.appdomain.cloud"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return HttpResponse(dealer_names)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
