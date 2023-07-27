06cf1086355aa1bf016b6815a281b9d67cbee322
Sebastian Kotulla <82386236+DerSimulant@users.noreply.github.com>
2023-07-27 18:41:36 +0200

    Update restapis.py
    first not yet working draft

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,12 +1,40 @@
 import requests
 import json
-# import related models here
+from .models import CarDealer
 from requests.auth import HTTPBasicAuth
+
 
 
 # Create a `get_request` to make HTTP GET requests
 # e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
 #                                     auth=HTTPBasicAuth('apikey', api_key))
+def get_request(url, api_key, **kwargs):
+    print(kwargs)
+    print("GET from {} ".format(url))
+    response = None  # Declare response here
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(
+            url=url, 
+            headers={'Content-Type': 'application/json', 'apikey': api_key},
+            params=kwargs
+        )
+    except:
+        # If any error occurs
+        print("Network exception occurred")
+    
+    if response:
+        status_code = response.status_code
+        print("With status {} ".format(status_code))
+        
+        if status_code == 200:
+            # If request was successful, load response data into a JSON format
+            json_data = json.loads(response.text)
+            return json_data
+    
+    # If request was not successful or an exception occurred, return an error message
+    return {"error": "Request failed or network exception occurred"}
+
 
 
 # Create a `post_request` to make HTTP POST requests
@@ -14,9 +42,29 @@ from requests.auth import HTTPBasicAuth
 
 
 # Create a get_dealers_from_cf method to get dealers from a cloud function
-# def get_dealers_from_cf(url, **kwargs):
-# - Call get_request() with specified arguments
-# - Parse JSON results into a CarDealer object list
+def get_dealers_from_cf(url, **kwargs):
+    results = []
+    # Call get_request with a URL parameter
+    json_result = get_request(url="https://eu-de.functions.appdomain.cloud/api/v1/web/5f6babc9-6519-4062-ac30-e2b3b37a1776/dealership-package/get-dealership", api_key="o5POKqlisry_eOetmb67wUNJKxRje-pkVOZ3zeKr34_t")
+    if json_result:
+        # Get the row list in JSON as dealers
+       if 'rows' in json_result:
+            # Get the row list in JSON as dealers
+            dealers = json_result["rows"]
+            # For each dealer object
+            for dealer in dealers:
+                # Get its content in `doc` object
+                dealer_doc = dealer["doc"]
+                # Create a CarDealer object with values in `doc` object
+                dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
+                                    id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+                                    short_name=dealer_doc["short_name"],
+                                    st=dealer_doc["st"], zip=dealer_doc["zip"])
+                results.append(dealer_obj)
+       else:
+            print("'rows' key not found in json_result")
+
+    return results
 
 
 # Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
